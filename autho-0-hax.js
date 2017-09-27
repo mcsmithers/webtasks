8c914419d858db58b65aa9017857a7378ad3f856
Christina Smithers <christina.smithers@gmail.com>
2017-09-27 10:37:39 -0700

    Create autho-0-hax.js

---
+++ autho-0-hax.js
@@ -0,0 +1,23 @@
+module.exports = function(context, cb) {
+  cb(null, {greeting: context.data.name || 'Here is a list of AWS prices in the east region' });
+  
+  var request = require('request');
+
+  for (var aws_price in aws_prices) {
+    var all_prices = price_by_region[region];
+    
+    request({
+      method: 'GET',
+      url: 'https://pricing.us-east-1.amazonaws.com/offers/v1.0/aws/index.json', 
+      headers: {
+        region: '',
+        price: ''
+      },
+      data: all_prices
+    }, function (error, res, body) {
+      // process error
+    });
+  }
+  
+  cb(null, {aws_price: context.data.price, region: context.data.region});
+};
